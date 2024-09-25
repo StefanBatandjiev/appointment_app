99576d3b140022b0cc2b9a7b61864bc264419ddd
Stefan <stefan.batandjiev@gmail.com>
2024-09-25 14:25:10 +0200

    Fixes

--- app/Filament/Widgets/CalendarWidget.php
+++ app/Filament/Widgets/CalendarWidget.php
@@ -101,18 +101,11 @@ class CalendarWidget extends BaseCalendarWidget
                             'total_price' => $reservation->getTotalPriceAttribute(),
                             'status' => $reservation->getReservationStatusAttribute(),
                             'icon' => match ($reservation->getReservationStatusAttribute()) {
-                                ReservationStatus::ONGOING => 'vendor/blade-heroicons/o-minus-circle.svg',
-                                ReservationStatus::SCHEDULED => 'vendor/blade-heroicons/o-clock.svg',
-                                ReservationStatus::FINISHED => 'vendor/blade-heroicons/o-check-circle.svg',
-                                ReservationStatus::CANCELED => 'vendor/blade-heroicons/o-x-circle.svg',
-                                ReservationStatus::PENDING_FINISH => 'vendor/blade-heroicons/o-exclamation-circle.svg',
-                            },
-                            'iconColor' => match ($reservation->getReservationStatusAttribute()) {
-                                ReservationStatus::ONGOING => 'filter: brightness(0) saturate(100%) invert(79%) sepia(66%) saturate(2254%) hue-rotate(352deg) brightness(103%) contrast(104%);',
-                                ReservationStatus::SCHEDULED => 'filter: brightness(0) saturate(100%) invert(53%) sepia(20%) saturate(1649%) hue-rotate(81deg) brightness(94%) contrast(88%);',
-                                ReservationStatus::FINISHED => 'filter: brightness(0) saturate(100%) invert(31%) sepia(28%) saturate(6136%) hue-rotate(200deg) brightness(104%) contrast(105%);',
-                                ReservationStatus::CANCELED => 'filter: brightness(0) saturate(100%) invert(38%) sepia(68%) saturate(5599%) hue-rotate(337deg) brightness(90%) contrast(90%);',
-                                ReservationStatus::PENDING_FINISH => 'filter: brightness(0) saturate(100%) invert(46%) sepia(17%) saturate(241%) hue-rotate(167deg) brightness(94%) contrast(86%);',
+                                ReservationStatus::ONGOING => svg('heroicon-o-minus-circle', 'w-6 h-6', ['style' => 'filter: brightness(0) saturate(100%) invert(79%) sepia(66%) saturate(2254%) hue-rotate(352deg) brightness(103%) contrast(104%);'])->toHtml(),
+                                ReservationStatus::SCHEDULED => svg('heroicon-o-clock', 'w-6 h-6', ['style' => 'filter: brightness(0) saturate(100%) invert(53%) sepia(20%) saturate(1649%) hue-rotate(81deg) brightness(94%) contrast(88%);'])->toHtml(),
+                                ReservationStatus::FINISHED => svg('heroicon-o-check-circle', 'w-6 h-6', ['style' => 'filter: brightness(0) saturate(100%) invert(31%) sepia(28%) saturate(6136%) hue-rotate(200deg) brightness(104%) contrast(105%);'])->toHtml(),
+                                ReservationStatus::CANCELED => svg('heroicon-o-x-circle', 'w-6 h-6', ['style' => 'filter: brightness(0) saturate(100%) invert(38%) sepia(68%) saturate(5599%) hue-rotate(337deg) brightness(90%) contrast(90%);'])->toHtml(),
+                                ReservationStatus::PENDING_FINISH => svg('heroicon-o-exclamation-circle', 'w-6 h-6', ['style' => 'filter: brightness(0) saturate(100%) invert(46%) sepia(17%) saturate(241%) hue-rotate(167deg) brightness(94%) contrast(86%);'])->toHtml(),
                             }
                         ]);
                 }
@@ -149,12 +142,23 @@ class CalendarWidget extends BaseCalendarWidget
             ->toArray();
     }
 
+//    public function onEventClick(array $info = [], ?string $action = null): void
+//    {
+//        $status = data_get($info, 'event.extendedProps.status');
+//
+//
+//        if ($status === ReservationStatus::FINISHED) {
+//            $this->defaultEventClickAction = 'ViewReservation';
+//        }
+//
+//    }
+
     public function getEventClickContextMenuActions(): array
     {
         return [
             self::viewAction(),
             self::editAction(),
-            $this->deleteAction()
+            $this->deleteAction(),
         ];
     }
 
@@ -309,7 +313,7 @@ class CalendarWidget extends BaseCalendarWidget
                 $this->refreshRecords();
 
                 Notification::make()
-                    ->title('Reservation Created')
+                    ->title('Reservation Edited')
                     ->success()
                     ->send();
             });
@@ -324,7 +328,7 @@ class CalendarWidget extends BaseCalendarWidget
                     Placeholder::make('status')
                         ->label(new HtmlString('<span class="text-lg font-extralight text-center">Status</span>'))
                         ->content(function (Reservation $record): HtmlString {
-                        $svg = match ($record->status) {
+                        $svg = match ($record->getReservationStatusAttribute()) {
                             ReservationStatus::ONGOING => svg('heroicon-o-minus-circle', 'w-6 h-6', ['style' => 'filter: brightness(0) saturate(100%) invert(79%) sepia(66%) saturate(2254%) hue-rotate(352deg) brightness(103%) contrast(104%);'])->toHtml(),
                             ReservationStatus::SCHEDULED => svg('heroicon-o-clock', 'w-6 h-6', ['style' => 'filter: brightness(0) saturate(100%) invert(53%) sepia(20%) saturate(1649%) hue-rotate(81deg) brightness(94%) contrast(88%);'])->toHtml(),
                             ReservationStatus::FINISHED => svg('heroicon-o-check-circle', 'w-6 h-6', ['style' => 'filter: brightness(0) saturate(100%) invert(31%) sepia(28%) saturate(6136%) hue-rotate(200deg) brightness(104%) contrast(105%);'])->toHtml(),
@@ -334,7 +338,7 @@ class CalendarWidget extends BaseCalendarWidget
 
                         return new HtmlString("
                             <div class=\"flex items-center\">
-                                <span class=\"px-1 text-lg\">{$record->status->value}</span>
+                                <span class=\"px-1 text-lg\">{$record->getReservationStatusAttribute()->value}</span>
                                 {$svg}
                             </div>
                         ");
@@ -530,6 +534,14 @@ class CalendarWidget extends BaseCalendarWidget
 
     public function authorize($ability, $arguments = [])
     {
+        if (($arguments[0]['status'] === ReservationStatus::FINISHED || $arguments[0]['end_time'] <= now()->timezone('GMT+2'))  && ($ability === 'delete' || $ability === 'EditReservation')) {
+            $this->cachedContextMenuActions['eventClick'][2]->hidden(true);
+            $this->cachedContextMenuActions['eventClick'][2]->disabled(true);
+            $this->cachedContextMenuActions['eventClick'][2]->visible(false);
+            $this->cachedContextMenuActions['eventClick'][1]->hidden(true);
+            $this->cachedContextMenuActions['eventClick'][1]->disabled(true);
+            $this->cachedContextMenuActions['eventClick'][1]->visible(false);
+        }
         return true;
     }
 }
--- resources/views/components/calendar/events/event.blade.php
+++ resources/views/components/calendar/events/event.blade.php
@@ -1,69 +1,59 @@
 <div class="py-0.5 px-2" role="region" aria-labelledby="event-details">
 
     <template x-if="view.type === 'resourceTimeGridWeek' && event.title !== 'Break Time'">
-        <img
-            :src="event.extendedProps.icon"
-            width="25"
-            height="25"
-            :style="event.extendedProps.iconColor"
-            alt="Status Icon"
-        />
+        <span x-html="event.extendedProps.icon"></span>
     </template>
 
     <template x-if="view.type === 'dayGridMonth' && event.title !== 'Break Time'">
-        <img
-            :src="event.extendedProps.icon"
-            width="5"
-            height="5"
-            :style="event.extendedProps.iconColor"
-            alt="Status Icon"
-        />
+        <span x-html="event.extendedProps.icon" class="svg-container"></span>
     </template>
 
     <template x-if="view.type === 'resourceTimeGridDay'">
-        <div class="flex flex-row flex-wrap justify-between" id="event-details">
-            <div class="flex flex-col justify-between">
-                <div class="flex flex-row items-center">
+        <div class="flex flex-col flex-wrap" id="event-details">
+            <div class="flex flex-row justify-between">
+                <div class="flex flex-col items-start">
                     <span x-text="timeText" class="font-semibold text-md"></span>
 
-                    <template x-if="event.title !== 'Break Time'">
-                        <span x-text="event.extendedProps.status" class="text-sm"
-                              style="padding-left: 20px; padding-right: 5px"></span>
-                    </template>
+                    <div class="flex items-center">
+                        <template x-if="event.title !== 'Break Time'">
+                            <span x-text="event.extendedProps.status" class="text-sm"></span>
+                        </template>
 
-                    <template x-if="event.title !== 'Break Time'">
-                        <img
-                            :src="event.extendedProps.icon"
-                            width="25"
-                            height="25"
-                            :style="event.extendedProps.iconColor"
-                            alt="Status Icon"
-                        />
-                    </template>
-
+                        <template x-if="event.title !== 'Break Time'">
+                            <span x-html="event.extendedProps.icon"></span>
+                        </template>
+                    </div>
                 </div>
-                <template x-if="event.title !== 'Break Time'">
-                    <span>
-                    Client:
-                    <span class="font-semibold text-md" x-text="event.extendedProps.client || 'N/A'"></span>
-                </span>
-                </template>
-                <template x-if="event.title !== 'Break Time'">
-                    <span>
-                    Assigned User:
-                    <span class="font-semibold text-md" x-text="event.extendedProps.assigned_user || 'N/A'"></span>
-                </span>
-                </template>
-            </div>
-            <div class="flex flex-col items-end">
-                <span x-text="event.title" class="text-xs"></span>
-                <template x-if="event.title !== 'Break Time'">
+                <div class="flex flex-col items-end">
+                    <span x-text="event.title" class="text-xs text-end" style="width: 150px"></span>
+                    <template x-if="event.title !== 'Break Time'">
                     <span class="text-base font-semibold">
                         <span x-text="event.extendedProps.total_price || 0"></span> MKD
                     </span>
+                    </template>
+                </div>
+            </div>
+            <div class="flex">
+                <template x-if="event.title !== 'Break Time'">
+                        <span>
+                            Client:
+                            <span class="font-semibold text-md" x-text="event.extendedProps.client || 'N/A'"></span>
+                        </span>
+                </template>
+                <template x-if="event.title !== 'Break Time'">
+                        <span class="px-2">
+                            Assigned User:
+                            <span class="font-semibold text-md" x-text="event.extendedProps.assigned_user || 'N/A'"></span>
+                        </span>
                 </template>
             </div>
         </div>
     </template>
 </div>
 
+<style>
+    .svg-container svg {
+        width: 10px;
+        height: 10px;
+    }
+</style>
